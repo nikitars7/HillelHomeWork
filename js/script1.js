b8f13c36924ba7be0a54246eb1467ee91d6e7020
Nikita <nikita_rs7@ukr.net>
2022-10-13 03:12:02 +0300

    second

--- js/script1.js
+++ js/script1.js
@@ -1,6 +1,12 @@
 "use strict";
-
+let homeWork = document.querySelector('.homework__text');
 
 const name = prompt("What's your name?", "");
-
 alert(`Hello , ${name} ! How are you?`);
+
+if (name === '') {
+   homeWork.innerHTML = "Безымянный)";
+} else {
+   homeWork.innerHTML = `${name}`;
+}
+
